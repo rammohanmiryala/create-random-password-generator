f65cf113bcefaf34354bd020456427679236f547
rammohanmiryala <rammohanmiryala@hotmail.com>
2021-09-23 22:29:21 +0930

    final update added comments and texted the output

---
+++ readme.md
@@ -0,0 +1,20 @@
+# create random password
+
+## About The Project
+
+This project is to generate a random password based on the selection criteria of( upper,lower ,symbol and number)
+
+
+## acceptance
+
+- this will generate random password based on the selection criteria of ( upper,lower ,symbol and number)
+
+
+# final work
+
+The following appearance of the site:
+
+![my portfolio](./assets/website.gif)
+
+# image reverence
+
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var symbol = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\"," ^ ",_", "{", "|", "}", "~"]
-
-// passwordtext = querySelector('#password')
-// passwordtext.value = password.value;
+var password = "";
+var availablechars = []
 
 function writePassword() {
   // takeing the length of the password
@@ -38,7 +38,7 @@ function writePassword() {
     if (upperCaseconf == false && lowerCaseconf == false && symbolSconf == false && numberconf == false) {
       window.confirm(" at least one option should be selected")
     } else {
-      var availablechars = []
+      //concting(combing) the selected chararters 
       if (upperCaseconf == true) {
         availablechars = availablechars.concat(uppercase)
       }
@@ -51,14 +51,16 @@ function writePassword() {
       if (numberconf == true) {
         availablechars = availablechars.concat(number)
       }
-
-      var password = "";
       for (i = 1; i <= passwordLength; i++) {
         var index = Math.floor(Math.random() * availablechars.length);
         password += availablechars[index];
       }
     }
   }
+   //retun statment that displays text on html page   
+  document.querySelector('#password').value = password;
+  console.log("Password", password);
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// listener to generate button
+generateBtn.addEventListener("click", writePassword)
